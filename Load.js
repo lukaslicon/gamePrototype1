9c4cee52ac90592fbe74afb9d5ae657e1d856f8b
Lukas Licon <lukas@licons.com>
2023-05-18 20:56:34 -0700

    random changes
    Co-authored-by: Jentsen <Jentsen@users.noreply.github.com>

--- Load.js
+++ Load.js
@@ -5,25 +5,17 @@ class load extends Phaser.Scene {
     preload ()
     {
         this.load.path = 'assets/';
-        this.load.image('tiles', 'assets/Overworld.png');
-        this.load.tilemapTiledJSON('map', 'assets/map.json');
+        
+        this.load.tilemapTiledJSON('map', 'map.json');
+        this.load.image('tileset', tilesetImage);
+
+        this.load.image('tiles', 'Overworld.png');
+        this.load.tilemapTiledJSON('OverworldJson', 'Overworld.json' );
+
     }
     create()
     {
-        // create a tilemap from the loaded json file
-        const map = this.make.tilemap({ key: 'map' });
-
-        // add tileset image to map
-        const tileset = map.addTilesetImage('Overworld', 'tiles');
-
-        // create tilemap layer using tileset + map data
-        const layer = map.createLayer('layer_name', tileset, 0, 0);
-
-
-        this.scene.start('intro');
+        this.scene.start('scene2');
     }
-    update()
-    {
 
-    }
 }
